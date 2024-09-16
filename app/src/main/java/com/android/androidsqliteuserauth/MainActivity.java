ab00be6d530ea9040f086ed8c55f4405ff449c07
kryp <zufairaslam1@gmail.com>
2024-09-17 01:23:16 +0530

    first commit

---
+++ app/src/main/java/com/android/androidsqliteuserauth/MainActivity.java
@@ -0,0 +1,27 @@
+package com.android.androidsqliteuserauth;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.myButton);
+        btn.setOnClickListener(v-> {
+            Toast.makeText(MainActivity.this, "test", Toast.LENGTH_LONG).show();
+        });
+
+    }
+}
